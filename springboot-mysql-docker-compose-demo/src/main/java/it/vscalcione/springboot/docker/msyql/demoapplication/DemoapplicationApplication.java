842fb4e50409ee9ab89c17d0f2e178b08d82fb1f
DIR\vincenzo.scalcione <vincenzo.scalcione@accenture.com>
2021-09-02 13:27:44 +0200

    Added springboot backend module

---
+++ springboot-mysql-docker-compose-demo/src/main/java/it/vscalcione/springboot/docker/msyql/demoapplication/DemoapplicationApplication.java
@@ -0,0 +1,13 @@
+package it.vscalcione.springboot.docker.msyql.demoapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoapplicationApplication.class, args);
+	}
+
+}
